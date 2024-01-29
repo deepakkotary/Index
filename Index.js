442b041352b947455d5984863a5e86fe8afb607e
deepakkotary <deepakkotary.9166@gmail.com>
2024-01-29 22:11:35 +0530

    updates

---
+++ Index.js
@@ -0,0 +1,9 @@
+// Registration of service worker 
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw js').then(reg=>{
+       console.log('registered', reg);
+  })   
+};
+
+
+
